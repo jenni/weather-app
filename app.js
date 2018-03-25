598ad32290977da74a63730b0f97f4c5d3910355
Jen <jen.lijo@zalando.de>
2018-03-25 22:39:32 +0200

    Grab address, latitude and longitude from weather API

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20schudomastr%20berlin&key=AIzaSyCg9x6K4-sf44jIWZV4xrNsViADbOPX_tA',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2))
+  console.log(`Address: ${body.results[0].formatted_address}`)
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`)
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`)
 })
-
