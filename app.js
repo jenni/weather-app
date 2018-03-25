bfde8f9abcb3afb991d188f8bb2740f6e3809d7c
Jen <jen.lijo@zalando.de>
2018-03-25 21:48:12 +0200

    first request call

---
+++ app.js
@@ -0,0 +1,9 @@
+const request = require('request')
+
+request({
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20schudomastr%20berlin',
+  json: true
+}, (error, response, body) => {
+  console.log(body)
+})
+
