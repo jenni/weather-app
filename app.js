0f492d56243d8da08b53415329f3a7769900e8db
Jen <jen.lijo@zalando.de>
2018-03-25 22:05:17 +0200

    Pretty print object response

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20schudomastr%20berlin',
   json: true
 }, (error, response, body) => {
-  console.log(body)
+  console.log(JSON.stringify(body, undefined, 2))
 })
 
