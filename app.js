7c1394824c0238f30450cc878a2b1149d184614c
Jen <jen.lijo@zalando.de>
2018-03-25 22:06:24 +0200

    Add google maps API key

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20schudomastr%20berlin',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=25%20schudomastr%20berlin&key=AIzaSyCg9x6K4-sf44jIWZV4xrNsViADbOPX_tA',
   json: true
 }, (error, response, body) => {
   console.log(JSON.stringify(body, undefined, 2))
